3d6c03093f63d6b64d63db357490a8311bfb7120
Varun Parthasarathy <vprthsr2@illinois.edu>
2017-07-16 19:01:52 -0500

    added web crawler

--- pagerank_crawler.py
+++ pagerank_crawler.py
@@ -1,34 +1,23 @@
 import sys
 
-from django.core.validators import URLValidator
-from django.core.exceptions import ValidationError
-
-from html.parser import HTMLParser
-from urllib.request import urlopen
-from urllib import parse
+from web_crawler import is_valid_url, Crawler
 
 
 def main(argv):
     try:
         url, depth = process_args(argv)
-    except:
+    except AssertionError:
         return
 
-    pass
+    print("Running crawl on '{}' with depth '{}'.".format(url, depth))
+    crawler = Crawler(url, depth)
+    page_map = crawler.get_map()
+    print(page_map)
 
 
 def print_usage():
     with open('usage.txt', 'r') as usage:
         print(usage.read())
-
-
-def is_valid_url(str):
-    val = URLValidator()
-    try:
-        val(str)
-    except ValidationError:
-        return False
-    return True
 
 
 def process_args(argv):
